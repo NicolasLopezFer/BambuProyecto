d86f0768cd073d69a2744d7f442dcb2260298571
Geraldine <avocadoenterprise@gmail.com>
2020-04-14 02:17:02 -0500

    Creación Clase Ouput

--- bambu/src/main/java/com/panda/bambu/model/inventory/Output.java
+++ bambu/src/main/java/com/panda/bambu/model/inventory/Output.java
@@ -1,5 +1,49 @@
 package com.panda.bambu.model.inventory;
 
-public class Output {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.panda.bambu.model.article.Article;
+
+@Entity
+public class Output {
+     
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+    private int id;
+    
+    @NotNull(message="Code is compulsory")
+	@Column(name = "code")
+    private String code;
+    
+    @NotNull(message="Date is compulsory")
+    @Column(name = "date")
+    private LocalDate date;
+
+    @NotNull(message="Detail is compulsory")
+    @Column(name = "detail")
+    private String detail;
+    
+    @NotNull(message="Quantity is compulsory")
+	@Column(name = "quanity")
+    private String quanity;
+    
+    @NotNull(message="Unit Cost is compulsory")
+	@Column(name = "unit_cost")
+    private double unitCost;
+
+    @NotNull(message="Total Cost is compulsory")
+	@Column(name = "total_cost")
+    private double totalCost;
+    
+    @ManyToOne()
+    private Article article;
 }
