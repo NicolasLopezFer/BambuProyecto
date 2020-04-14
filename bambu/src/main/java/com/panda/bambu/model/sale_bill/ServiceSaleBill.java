70c2a6add0fb480f0a81205af5403c1936d49e9d
Geraldine <avocadoenterprise@gmail.com>
2020-04-14 02:52:55 -0500

    Creacion Métodos Clases

--- bambu/src/main/java/com/panda/bambu/model/sale_bill/ServiceSaleBill.java
+++ bambu/src/main/java/com/panda/bambu/model/sale_bill/ServiceSaleBill.java
@@ -46,4 +46,140 @@ public class ServiceSaleBill {
     @Column(name = "total")
     private LocalDate total;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(String idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDate getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(LocalDate expiration) {
+        this.expiration = expiration;
+    }
+
+    public Set<ServiceSale> getServices() {
+        return services;
+    }
+
+    public void setServices(Set<ServiceSale> services) {
+        this.services = services;
+    }
+
+    public LocalDate getTotal() {
+        return total;
+    }
+
+    public void setTotal(LocalDate total) {
+        this.total = total;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((code == null) ? 0 : code.hashCode());
+        result = prime * result + ((customer_name == null) ? 0 : customer_name.hashCode());
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((expiration == null) ? 0 : expiration.hashCode());
+        result = prime * result + id;
+        result = prime * result + ((idCustomer == null) ? 0 : idCustomer.hashCode());
+        result = prime * result + ((services == null) ? 0 : services.hashCode());
+        result = prime * result + ((total == null) ? 0 : total.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ServiceSaleBill other = (ServiceSaleBill) obj;
+        if (code == null) {
+            if (other.code != null)
+                return false;
+        } else if (!code.equals(other.code))
+            return false;
+        if (customer_name == null) {
+            if (other.customer_name != null)
+                return false;
+        } else if (!customer_name.equals(other.customer_name))
+            return false;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (expiration == null) {
+            if (other.expiration != null)
+                return false;
+        } else if (!expiration.equals(other.expiration))
+            return false;
+        if (id != other.id)
+            return false;
+        if (idCustomer == null) {
+            if (other.idCustomer != null)
+                return false;
+        } else if (!idCustomer.equals(other.idCustomer))
+            return false;
+        if (services == null) {
+            if (other.services != null)
+                return false;
+        } else if (!services.equals(other.services))
+            return false;
+        if (total == null) {
+            if (other.total != null)
+                return false;
+        } else if (!total.equals(other.total))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceSaleBill [code=" + code + ", customer_name=" + customer_name + ", date=" + date + ", expiration="
+                + expiration + ", id=" + id + ", idCustomer=" + idCustomer + ", services=" + services + ", total="
+                + total + "]";
+    }
+ 
+
 }
