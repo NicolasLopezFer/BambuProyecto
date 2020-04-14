c7805e3625ec5927982b75a27712164d935fb5a6
Geraldine <avocadoenterprise@gmail.com>
2020-04-14 02:16:36 -0500

    Creación Clase Inventory

--- bambu/src/main/java/com/panda/bambu/model/inventory/Inventory.java
+++ bambu/src/main/java/com/panda/bambu/model/inventory/Inventory.java
@@ -1,5 +1,41 @@
 package com.panda.bambu.model.inventory;
 
+import java.time.LocalDate;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+
+
+@Entity
 public class Inventory {
+     
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+    private int id;
+    
+    @NotNull(message="Date is compulsory")
+    @Column(name = "date")
+    private LocalDate date;
+
+    @ManyToMany()
+    private MeasureMethod measureMethod;
+
+    @OneToMany()
+    private Set<Entry> entries;
+    
+    @OneToMany()
+    private Set<Output> outputs;
+    
+    @OneToMany()
+    private Set<Balance> balances;
 
 }
