6a206bf953c6951ed5f9a55d9d51828f55ceea7b
Geraldine <avocadoenterprise@gmail.com>
2020-04-14 02:16:23 -0500

    Creacion Clase EntryRepository

--- bambu/src/main/java/com/panda/bambu/model/inventory/EntryRepository.java
+++ bambu/src/main/java/com/panda/bambu/model/inventory/EntryRepository.java
@@ -1,5 +1,9 @@
 package com.panda.bambu.model.inventory;
 
-public class EntryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EntryRepository extends JpaRepository<Entry, Long> {
 
 }
