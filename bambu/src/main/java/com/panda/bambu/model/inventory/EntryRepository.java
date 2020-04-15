e741fbff235285f3c2aa1e35c89431ea4526ceaf
Geraldine <avocadoenterprise@gmail.com>
2020-04-15 03:04:52 -0500

    Creación Servicios Entradas y Salidas

--- bambu/src/main/java/com/panda/bambu/model/inventory/EntryRepository.java
+++ bambu/src/main/java/com/panda/bambu/model/inventory/EntryRepository.java
@@ -5,5 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, Long> {
-
+      
+       public Entry findByCode(String code);
 }
--- bambu/src/main/java/com/panda/bambu/service/article/ArticleService.java
+++ bambu/src/main/java/com/panda/bambu/service/article/ArticleService.java
@@ -1,10 +1,24 @@
 package com.panda.bambu.service.article;
 
+import com.panda.bambu.model.article.Article;
+import com.panda.bambu.model.article.ArticleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticleService {
       
+    @Autowired
+    private ArticleRepository articleRepository;   
     
+    public Article findById(Long id){
+        return articleRepository.findById(id).get();
+    }
+
+    public Article findByCode(String code){
+        return articleRepository.findByCode(code);
+    }
+   
 
 }
