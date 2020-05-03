9bfe9570f54d1f5c980b018539ad8673223074e9
Nicolas Lopez <nlopez97f@gmail.com>
2020-05-02 22:56:01 -0500

    Controller servicio

---
+++ bambu/src/main/java/com/panda/bambu/controller/ServiceController.java
@@ -0,0 +1,25 @@
+package com.panda.bambu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ServiceController {
+
+    // @Autowired
+    // ServiceService serviceService;
+
+    // @Autowired
+    // ServiceRepository serviceRepository;
+
+
+    // @RequestMapping(value = "/service", method = RequestMethod.GET)
+	// public ModelAndView service(@RequestParam(defaultValue="0") int page) {
+	// 	ModelAndView modelAndView = new ModelAndView();
+	// 	modelAndView.addObject("serviciosEmpresa",serviceRepository.findAll(/*PageRequest.of(page, 10)*/));
+	// 	modelAndView.addObject("paginaActual",page);
+	// 	modelAndView.setViewName("servicio"); // resources/template/articulos.html
+	// 	return modelAndView;
+	// }
+
+}
