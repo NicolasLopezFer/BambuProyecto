06c6de87ab9cc897aebe3c1dbabf3685d96a2ee0
David López <dadavivi88@hotmail.com>
2020-05-07 17:01:52 -0500

    Front egreso

--- bambu/src/main/java/com/panda/bambu/controller/EgresosController.java
+++ bambu/src/main/java/com/panda/bambu/controller/EgresosController.java
@@ -2,6 +2,7 @@ package com.panda.bambu.controller;
 
 import java.util.Optional;
 
+import com.panda.bambu.model.egreso.Egreso;
 import com.panda.bambu.service.egreso.EgresoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,48 @@ public class EgresosController
     @RequestMapping(value = "/egreso", method = RequestMethod.GET)
 	public ModelAndView egresos(@RequestParam(defaultValue="0") int page) {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("egreso", egresoService.findAll());
+		modelAndView.addObject("egresos", egresoService.findAll());
 		modelAndView.setViewName("egreso"); // resources/template/egresos.html
 		return modelAndView;
+    }
+    
+    @RequestMapping(value = "/egreso-crear", method = RequestMethod.POST)
+	public ModelAndView crearArticulo(Egreso a) 
+	{
+		ModelAndView modelAndView = new ModelAndView();
+		if(egresoService.create(a)) {
+			modelAndView.addObject("responseMessage", "Articulo guardado Exitosamente!");	
+			System.out.println("Articulo guardado Exitosamente!");
+		}else {
+			modelAndView.addObject("responseMessage", "Existen errores al guardar el articulo");
+			System.out.println("NO SE GUARDO");
+		}
+		modelAndView.setViewName("redirect:/egreso");
+		
+		return modelAndView;
+    }
+
+    @RequestMapping(value = "/egreso-borrar", method = RequestMethod.GET)
+	public ModelAndView borrarReciboCaja(long id) {
+		ModelAndView modelAndView = new ModelAndView();
+		if(egresoService.delete(egresoService.findById(id))) 
+		{
+			modelAndView.addObject("responseMessage", "Articulo borrado Exitosamente!");	
+		}else {
+			modelAndView.addObject("responseMessage", "Existen errores al guardar el articulo");
+		}
+		modelAndView.setViewName("redirect:/egreso");
+		return modelAndView;
 	}
+
+
+    @RequestMapping(value = "/egreso-encontrarUno", method = RequestMethod.GET)
+	@ResponseBody
+	public Egreso encontrarReciboCaja(long id) 
+	{
+		return egresoService.findById(id);
+	}
+    
+
 
 }
