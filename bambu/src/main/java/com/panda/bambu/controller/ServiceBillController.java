1d7ad7d27cf1b98fd40ac4978f9c52e42ac7b265
Daniel Fernandez <dafeul@hotmail.com>
2020-05-18 21:34:32 -0500

    Se integro la factura de servicio

--- bambu/src/main/java/com/panda/bambu/controller/ServiceBillController.java
+++ bambu/src/main/java/com/panda/bambu/controller/ServiceBillController.java
@@ -33,85 +33,88 @@ public class ServiceBillController {
 
 	private double t = 0;
 
-     @Autowired
-	 ServiceFamiEmpresaService serviceFamiEmpresaService;
-	 
-	 @Autowired
-	 ServiceSaleService serSaleService;
+	@Autowired
+	ServiceFamiEmpresaService serviceFamiEmpresaService;
 
-	 @Autowired
-	 ServiceSaleBillService serSaleBillService;
-	 
-	 @RequestMapping(value = "/serviFacturaForm", method = RequestMethod.GET)
-	 public ModelAndView servicioHome() {
-		 final ModelAndView modelAndView = new ModelAndView();
-		 final List<ServiceSale> s = serSale;
-		 double total = 0;
-		 t = 0;
-		 for(int i = 0;i<s.size();i++){
+	@Autowired
+	ServiceSaleService serSaleService;
+
+	@Autowired
+	ServiceSaleBillService serSaleBillService;
+	
+	List<ServiceSaleBill> facturasServicio=new ArrayList<>();
+	
+	Boolean filtrar=false;
+
+	@RequestMapping(value = "/serviFacturaForm", method = RequestMethod.GET)
+	public ModelAndView servicioHome() {
+		final ModelAndView modelAndView = new ModelAndView();
+		final List<ServiceSale> s = serSale;
+		double total = 0;
+		t = 0;
+		for (int i = 0; i < s.size(); i++) {
 			total = total + s.get(i).getTotalValue();
 			t = t + s.get(i).getTotalValue();
-		 }
-		 modelAndView.addObject("serviceSale", s);
-		 modelAndView.addObject("total", total);
-		 modelAndView.setViewName("serviFacturaForm"); 
-		 return modelAndView;
-	 }
+		}
+		modelAndView.addObject("serviceSale", s);
+		modelAndView.addObject("total", total);
+		modelAndView.setViewName("serviFacturaForm");
+		return modelAndView;
+	}
 
-	 @RequestMapping(value = "/guardarBill", method = RequestMethod.POST)
-	 public ModelAndView servicioHomePost(@RequestParam(value = "numFact") String code,
-										   @RequestParam(value = "nombreCliente") String nombre,
-										   @RequestParam(value = "nitcc") String nitcc,
-										   @RequestParam(value = "fechaInicio")String fechaInicio,
-										   @RequestParam(value = "fechaVenci")String fechaVenci) {
+	@RequestMapping(value = "/guardarBill", method = RequestMethod.POST)
+	public ModelAndView servicioHomePost(@RequestParam(value = "numFact") String code,
+			@RequestParam(value = "nombreCliente") String nombre, @RequestParam(value = "nitcc") String nitcc,
+			@RequestParam(value = "fechaInicio") String fechaInicio,
+			@RequestParam(value = "fechaVenci") String fechaVenci) {
 		ModelAndView modelAndView = new ModelAndView();
-		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		 LocalDate dateFactura = LocalDate.parse(fechaInicio,formatter);
-		 LocalDate dateVencimiento = LocalDate.parse(fechaVenci, formatter);
-		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate dateFactura = LocalDate.parse(fechaInicio, formatter);
+		LocalDate dateVencimiento = LocalDate.parse(fechaVenci, formatter);
+
 		// for(int i = 0; i < serSale.size();i++){
-		// 	serSaleService.create(serSale.get(i));
+		// serSaleService.create(serSale.get(i));
 		// }
 
 		Set<ServiceSale> sett = new HashSet<ServiceSale>(serSale);
 		serSaleBillService.create(code, nombre, dateFactura, dateVencimiento, sett, nitcc, t);
 
-		 System.out.println("A 2");
+		System.out.println("A 2");
 
-		 serSale.clear();
-		 t = 0;
-		 modelAndView.setViewName("redirect:/serviFacturaForm");	
-		 return modelAndView;
-	 }
+		serSale.clear();
+		t = 0;
+		modelAndView.setViewName("redirect:/serviFacturaForm");
+		return modelAndView;
+	}
 
-	 @RequestMapping(value = "/agregarServicio", method = RequestMethod.POST)
-	 public ModelAndView serviBill(@RequestParam(value = "code") String code,
-	 					   @RequestParam(value = "cantidad") String cantidad,
-	 					   @RequestParam(value = "descuento") String descuento) {
-		 ModelAndView modelAndView = new ModelAndView();
-		 ServiceSale ss = new ServiceSale();
-		 ServiceFamiEmpresa sfe = new ServiceFamiEmpresa();
-		 sfe = serviceFamiEmpresaService.findByCode(code);
-		 if(sfe != null){
+	@RequestMapping(value = "/agregarServicio", method = RequestMethod.POST)
+	public ModelAndView serviBill(@RequestParam(value = "code") String code,
+			@RequestParam(value = "cantidad") String cantidad, @RequestParam(value = "descuento") String descuento) {
+		ModelAndView modelAndView = new ModelAndView();
+		ServiceSale ss = new ServiceSale();
+		ServiceFamiEmpresa sfe = new ServiceFamiEmpresa();
+		sfe = serviceFamiEmpresaService.findByCode(code);
+		if (sfe != null) {
 			ss.setServiceFamiEmpresa(sfe);
-			 ss.setDiscount(Float.parseFloat(descuento));
-		 	ss.setQuantity(Float.parseFloat(cantidad));
-		 	ss.setTotalValue(serSaleService.calculateTotal(sfe,Integer.parseInt(cantidad),Float.parseFloat(descuento)));
-		 	serSale.add(ss); 
-		 }else{
-			modelAndView.addObject("mensaje","No existe tal servicio");
-		 }
-		 
-		 modelAndView.setViewName("redirect:/serviFacturaForm");					
-		 return modelAndView;
-	 }
+			ss.setDiscount(Float.parseFloat(descuento));
+			ss.setQuantity(Float.parseFloat(cantidad));
+			ss.setTotalValue(
+					serSaleService.calculateTotal(sfe, Integer.parseInt(cantidad), Float.parseFloat(descuento)));
+			serSale.add(ss);
+		} else {
+			modelAndView.addObject("mensaje", "No existe tal servicio");
+		}
 
-	 @RequestMapping(value = "/borrarServicio", method = RequestMethod.GET)
+		modelAndView.setViewName("redirect:/serviFacturaForm");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/borrarServicio", method = RequestMethod.GET)
 	public ModelAndView borrarArticulo(final float id) {
 		final ModelAndView modelAndView = new ModelAndView();
-		for(int i = 0;i<serSale.size();i++){
+		for (int i = 0; i < serSale.size(); i++) {
 			double code = serSale.get(i).getTotalValue();
-			if(code == id){
+			if (code == id) {
 				serSale.remove(i);
 			}
 		}
@@ -126,5 +129,45 @@ public class ServiceBillController {
 		t = 0;
 		modelAndView.setViewName("redirect:/emprendedor");
 		return modelAndView;
+	}
+
+	@RequestMapping(value = "/filtrarFechasFactura", method = RequestMethod.POST)
+	public ModelAndView filtrarFechasFactura(@RequestParam(value = "fechaInicio") String fechaInicio,
+			@RequestParam(value = "fechaFin") String fechaFin) {
+		return filtrarFechasFacturaServicioPost(fechaInicio, fechaFin);
+	}
+
+	@RequestMapping(value = "/filtrarFechasFacturaServicio")
+	public ModelAndView filtrarFechasFacturaServicioPost(String fechaInicio, String fechaFin) {
+		ModelAndView modelAndView = new ModelAndView();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate dateInicial = LocalDate.parse(fechaInicio, formatter);
+		LocalDate dateFinal = LocalDate.parse(fechaFin, formatter);
+		List<ServiceSaleBill> list_facturas_servicio = new ArrayList<>();
+		for (ServiceSaleBill s : serSaleBillService.findAll()) {
+			for(ServiceSale serv: s.getServices()) {
+				System.out.println("Servicios "+serv.getServiceFamiEmpresa().getName());
+			}
+			if (s.getDate().isAfter(dateInicial) && s.getDate().isBefore(dateFinal))
+				list_facturas_servicio.add(s);
+		}
+		facturasServicio = list_facturas_servicio;
+		filtrar = true;
+		modelAndView.setViewName("redirect:/reporteFacturasServicio");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/reporteFacturasServicio", method = RequestMethod.GET)
+	public ModelAndView reporteFacturas(@RequestParam(defaultValue = "0") int page) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("listaFiltrada", facturasServicio);
+		modelAndView.setViewName("reporteFacturasServicio"); // resources/template/reporteEgresos.html
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/reporteFacturaServicio-encontrarUno", method = RequestMethod.GET)
+	@ResponseBody
+	public ServiceSaleBill encontrarFacturaServicio(long id) {
+		return serSaleBillService.findById(id);
 	}
 }
